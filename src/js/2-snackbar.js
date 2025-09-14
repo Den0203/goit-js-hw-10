2ed3747adbec463c53e1217443b51a13eb34d061
Denis Shulga <sulgadenis4@gmail.com>
2025-09-14 13:10:05 +0300

    add tasks

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const delay = Number(e.target.elements.delay.value);
+  const state = e.target.elements.state.value;
+  form.reset();
+  e.target.elements.delay.focus();
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: '✅ Success',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: '❌ Error',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
